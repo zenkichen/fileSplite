9ecc70a0d0b212e51fbda865e46fbed87d202046
zenkichen <zenkichen@gmail.com>
2015-04-27 11:45:26 +0800

    文件拆分
    用于E2PROM烧写

---
+++ fileSplite/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace fileSplite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSender = new System.Windows.Forms.Button();
+            this.btnSelFile = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIPaddr = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.btnSender);
+            this.groupBox1.Controls.Add(this.btnSelFile);
+            this.groupBox1.Controls.Add(this.txtFileName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "文件选项";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(208, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "直接发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(9, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(193, 12);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 50;
+            this.progressBar1.Visible = false;
+            // 
+            // btnSender
+            // 
+            this.btnSender.Enabled = false;
+            this.btnSender.Location = new System.Drawing.Point(208, 65);
+            this.btnSender.Name = "btnSender";
+            this.btnSender.Size = new System.Drawing.Size(75, 23);
+            this.btnSender.TabIndex = 3;
+            this.btnSender.Text = "拆分发送";
+            this.btnSender.UseVisualStyleBackColor = true;
+            this.btnSender.Click += new System.EventHandler(this.btnSender_Click);
+            // 
+            // btnSelFile
+            // 
+            this.btnSelFile.Enabled = false;
+            this.btnSelFile.Location = new System.Drawing.Point(208, 34);
+            this.btnSelFile.Name = "btnSelFile";
+            this.btnSelFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelFile.TabIndex = 2;
+            this.btnSelFile.Text = "选取文件";
+            this.btnSelFile.UseVisualStyleBackColor = true;
+            this.btnSelFile.Click += new System.EventHandler(this.btnSelFile_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(9, 36);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(193, 21);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.btnDisconnect);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtPort);
+            this.groupBox2.Controls.Add(this.txtIPaddr);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "环境设置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "下行端口号";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 21);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "4001";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(148, 74);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 6;
+            this.btnDisconnect.Text = "断开";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(67, 74);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "上行端口号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "远程主机IP";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(122, 37);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(75, 21);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "4002";
+            // 
+            // txtIPaddr
+            // 
+            this.txtIPaddr.Location = new System.Drawing.Point(9, 37);
+            this.txtIPaddr.Name = "txtIPaddr";
+            this.txtIPaddr.Size = new System.Drawing.Size(107, 21);
+            this.txtIPaddr.TabIndex = 3;
+            this.txtIPaddr.Text = "192.168.0.121";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "下行数据";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(314, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(434, 400);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 443);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSelFile;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSender;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIPaddr;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ fileSplite/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace fileSplite
+{
+    public partial class Form1 : Form
+    {
+        private const ushort CRC16_POLYNOMIAL = 0x1021; /*CRC_16校验方式的多项式*/
+        private const byte Cdd = 0xDD;
+        private const byte C60 = 0x60;
+        private const byte Caa = 0xAA;
+        private const byte C51 = 0x51;
+        private const byte C01 = 0x01;
+        private const byte C20 = 0x20;
+        private const ushort Cffff = 0xFFFF;
+
+        private ushort[] Table_CRC = new ushort[256];
+        private string file;
+        //private FileStream aFile, bFile;
+        private byte[] buff = new byte[60];
+        private byte[] pkgBuff = new byte[69];
+
+        private TcpClient tcpClient;
+        private TcpClient tcpDownClient=null;
+        private Boolean isListening = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CRC16Init();
+        }
+
+        private void btnSelFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*.*)|*.*";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = fileDialog.FileName;
+                txtFileName.Text = file;
+            }
+            btnSender.Enabled = true;
+        }
+
+        void CRC16Init()
+        {
+            uint i, j;
+            uint nData;
+            uint nAccum;
+
+            for (i = 0; i < 256; i = i + 1)
+            {
+                nData = (uint)(i << 8);
+                nAccum = 0;
+                for (j = 0; j < 8; j = j + 1)
+                {
+                    if (((nData ^ nAccum) & 0x8000) != 0)
+                        nAccum = (nAccum << 1) ^ (CRC16_POLYNOMIAL);
+                    else
+                        nAccum <<= 1;
+                    nData <<= 1;
+                }
+                Table_CRC[i] = (ushort)nAccum;
+            }
+        }
+
+        /**********************************************
+        * 反转数据的比特位, 反转后MSB为1. *
+        * 反转前: 1110100011101110 0010100111100000 *
+        * 反转后: 1111001010001110 1110001011100000 *
+        ***********************************************/
+        ulong CRCBitReflect(ulong ulData, int nBits)
+        {
+            ulong ulResult = 0x00000000L;
+            int n;
+
+            for (n = 0; n < nBits; n = n + 1)
+            {
+                if ((ulData & 0x00000001L) != 0)
+                {
+                    ulResult |= (ulong)(1L << ((nBits - 1) - n));
+                }
+                ulData = (ulData >> 1);
+            }
+            return (ulResult);
+        }
+
+        /*******************************************************
+        *以CRC_16方式计算一个数据块的CRC值.         *
+        *pucData - 待校验的数据块指针.                            *
+        *nBytes - 数据块大小, 单位是字节.                       *
+        *返回值是无符号的长整型, 其中低16位有效. *
+        *********************************************************/
+        byte[] CRC16Calc(byte[] aData, int aSize)
+        {
+            int i;
+            byte[] crc = new byte[2];
+            uint nAccum = 0xffff;
+            for (i = 0; i < aSize; i++)
+                nAccum = ((nAccum & 0x00FF) << 8) ^ (uint)Table_CRC[(nAccum >> 8) ^ aData[i]];
+            crc[0] = (byte)(nAccum & 0x00FF);
+            crc[1] = (byte)(nAccum >> 8);
+            return crc;
+        }
+
+        public static string ToHexString(byte[] bytes) // 0xae00cf => "AE00CF "
+        {
+            string hexString = string.Empty;
+            if (bytes != null)
+            {
+                StringBuilder strB = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    strB.Append(bytes[i].ToString("X2")+" ");
+                }
+                hexString = strB.ToString();
+            }
+            return hexString;
+        }
+
+        private void btnSender_Click(object sender, EventArgs e)
+        {
+            FileStream aFile;
+            ushort packCnt = 0,lengthTmp;
+            long i;
+            byte dataNull = 0x20;
+            byte[] inputBytes = new byte[65];
+            byte[] CRC = new byte[2];
+
+            aFile = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Read);
+            BinaryReader br = new BinaryReader(aFile);
+            //// 首先判断，文件是否已经存在
+            //if (File.Exists("test.o"))
+            //{
+            //    // 如果文件已经存在，那么删除掉.
+            //    File.Delete("test.o");
+            //}
+            //bFile = new FileStream("test.o", FileMode.OpenOrCreate, FileAccess.Write);
+            //BinaryWriter bw = new BinaryWriter(bFile);
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+            NetworkStream ns = tcpClient.GetStream();
+
+            //如果读取的文件小于60字节
+            while ((aFile.Length - aFile.Position) > 60)
+            {
+                inputBytes[0] = Caa;
+                inputBytes[1] = C51;
+                inputBytes[2] = C01;
+                inputBytes[3] = (byte)(packCnt >> 8);
+                inputBytes[4] = (byte)(packCnt & 0x00FF);
+                try
+                {
+                    for (i = 0; i < 60; i++)
+                    {
+                        inputBytes[5 + i] = br.ReadByte();
+                    }
+                }
+                catch (IOException ex)
+                {
+                }
+                CRC = CRC16Calc(inputBytes, inputBytes.Length);
+                //bw.Write(Cdd);//写至新文件
+                //bw.Write(C60);
+                //bw.Write(inputBytes, 0, 65);
+                //bw.Write(CRC[1]);
+                //bw.Write(CRC[0]);
+                packCnt++;
+                pkgBuff[0]=Cdd;//网络发出
+                pkgBuff[1]=C60;
+                for (i = 0; i < 65;i++ )
+                {
+                    pkgBuff[2 + i] = inputBytes[i];
+                }
+                pkgBuff[67] = CRC[1];
+                pkgBuff[68] = CRC[0];
+                ns.Write(pkgBuff, 0, 69);
+                Thread.Sleep(1000);
+                progressBar1.Value = (int)(aFile.Position * 100 / aFile.Length);//进度条
+            }
+            lengthTmp = (ushort)(aFile.Length - aFile.Position);//获取剩余字节数
+            inputBytes[0] = Caa;
+            inputBytes[1] = C51;
+            inputBytes[2] = C01;
+            inputBytes[3] = 0xFF;
+            inputBytes[4] = 0xFF;
+            try
+            {
+                for (i = 0; i < lengthTmp; i++)
+                {
+                    inputBytes[5 + i] = br.ReadByte();
+                }
+                for (; i < 60; i++)
+                {
+                    inputBytes[5 + i] = dataNull;
+                }
+            }
+            catch (IOException ex)
+            {
+            }
+            CRC = CRC16Calc(inputBytes, inputBytes.Length);
+            //bw.Write(Cdd);//写至新文件
+            //bw.Write(C60);
+            //bw.Write(inputBytes, 0, 65);
+            //bw.Write(CRC[1]);
+            //bw.Write(CRC[0]);
+            pkgBuff[0] = Cdd;//网络发出
+            pkgBuff[1] = C60;
+            for (i = 0; i < 65; i++)
+            {
+                pkgBuff[2 + i] = inputBytes[i];
+            }
+            pkgBuff[67] = CRC[1];
+            pkgBuff[68] = CRC[0];
+            ns.Write(pkgBuff, 0, 69);
+            Thread.Sleep(1000);
+            progressBar1.Value = (int)(aFile.Position * 100 / aFile.Length);//进度条
+
+            MessageBox.Show("完成");
+            progressBar1.Visible = false;
+            //bw.Close();
+            br.Close();
+            aFile.Close();
+            //bFile.Close();
+            ns.Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClientWithTimeout(txtIPaddr.Text, Int32.Parse(txtPort.Text), 5000).Connect();
+                //tcpDownClient = new TcpClientWithTimeout(txtIPaddr.Text, Int32.Parse(textBox1.Text), 5000).Connect();
+                btnDisconnect.Enabled = true;
+                btnConnect.Enabled = false;
+                btnSelFile.Enabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("连接超时！");
+            }
+            isListening = true;
+            Thread thread = new Thread(new ThreadStart(SocketListen));
+            thread.Start();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            tcpClient.Close();
+            //tcpDownClient.Close();
+            isListening = false;
+            btnDisconnect.Enabled = false;
+            btnConnect.Enabled = true;
+            btnSelFile.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream aFile;
+            aFile = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Read);
+            byte[] inputBytes = new byte[aFile.Length];
+            BinaryReader br = new BinaryReader(aFile);
+            NetworkStream ns = tcpClient.GetStream();
+            try
+            {
+                for (int i = 0; i < aFile.Length; i++)
+                {
+                    inputBytes[i] = br.ReadByte();
+                }
+                ns.Write(inputBytes, 0, (int)aFile.Length);
+            }
+            catch (IOException ex)
+            {
+            }
+            br.Close();
+            aFile.Close();
+            ns.Close();
+        }
+        protected delegate void UpdateDisplayDelegate(byte[] text);
+        public void SocketListen()
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(new IPEndPoint(IPAddress.Parse(txtIPaddr.Text), Int32.Parse(textBox1.Text)));
+            while (isListening)
+            {
+                listener.Listen(0);
+
+                Socket socket = listener.Accept();
+                Stream netStream = new NetworkStream(socket);
+                StreamReader reader = new StreamReader(netStream);
+
+                string result = reader.ReadToEnd();
+                Invoke(new UpdateDisplayDelegate(UpdateDisplay), new object[] { result });
+                socket.Close();
+            }
+            listener.Close();
+        }
+        public void UpdateDisplay(byte[] text)
+        {
+            richTextBox1.Text = ToHexString(text);
+        }
+    }
+}
